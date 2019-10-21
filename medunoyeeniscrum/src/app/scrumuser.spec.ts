8790dc4d70f7fa74b70482980bfdeff42ca1defd
Ennyola <medunoyeeni@gmail.com>
2019-10-21 10:02:06 +0100

    Signup functionaliy added

---
+++ medunoyeeniscrum/src/app/scrumuser.spec.ts
@@ -0,0 +1,9 @@
+import { Scrumuser } from './scrumuser';
+import { from } from 'rxjs';
+
+
+describe('Scrumuser', () => {
+  it('should create an instance', () => {
+    expect(new Scrumuser()).toBeTruthy();
+  });
+});
